7680bbe54d15b26d92600f948821050906774048
Zoe <zoebelleton@gmail.com>
2020-09-19 10:18:48 +0200

    Modal

---
+++ .history/web/index_20200919101841.php
@@ -0,0 +1,60 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+
+
+function get_tasks()
+{
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  // Use the database 
+  $conn = new mysqli($server, $username, $password, $db);
+
+  // Check connection
+  if ($conn->connect_error) 
+  {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $conn->set_charset("utf8");
+  return mysqli_fetch_all($conn->query("SELECT * FROM task"));
+}
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+    $tasks = get_tasks();
+    $app['monolog']->addDebug('logging output.');
+    return $app['twig']->render('index.twig', ['tasks' => $tasks]);
+  });
+
+
+  $app->get('/update_task', function() use($app) {
+    $app['monolog']->addDebug('logging output.');
+    return $this->json(['response' => 'success']);
+  });
+
+
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
+$app->run();
